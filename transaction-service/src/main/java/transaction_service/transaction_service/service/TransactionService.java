7f68339e81cf84ea25f9786930c9281ea3364791
AndreyGordienkotut <andreykagordienko14@gmail.com>
2026-02-21 22:06:19 +0200

    Adding the liquibase dependency and adding init-schema to each service

--- transaction-service/src/main/java/transaction_service/transaction_service/service/TransactionService.java
+++ transaction-service/src/main/java/transaction_service/transaction_service/service/TransactionService.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import transaction_service.transaction_service.dto.DepositRequestDto;
